6d219861237be1152990dcd61284bb75e66a7730
InM1nd <zabolotny1111@gmail.com>
2023-11-24 12:45:58 +0100

    new projets

---
+++ src/components/Main/projects/dashboard.jsx
@@ -0,0 +1,50 @@
+import styles from './projects.module.scss'
+// import Teple from '../../../img/other/123.jpg'
+import Teple from '../../../img/other/Dashboard.png'
+
+const Dashboard = () => {
+    return (
+      <section className={styles.section}>
+        <div className={styles.projects_wrapper}>
+              <div className={styles.projects_img_border}>
+                <div className={styles.projects_img_wrapper}>
+                    
+                      <img className={styles.projects_img} src={Teple} alt='PROJECT'/>
+                    
+                  </div>
+                </div>
+            <div  className={styles.projects_content}>
+            <div className={styles.projects_describ}>
+              <div className={styles.projects_describ_text}>
+                <div  className={styles.projects_describ_title}>DASHBOARD</div >
+                <div  className={styles.projects_describ_info}>CREATING A PRACTICAL FITNESS PLATFORM</div >
+              </div>
+            </div>   
+            <div className={styles.projects_filter}>
+            <a className={styles.projects_filter_button}>branding</a>
+                <a className={styles.projects_filter_button}>product dev</a>
+                <a className={styles.projects_filter_button}>communication</a>
+                <a className={styles.projects_filter_button}>strategy</a> 
+                <a className={styles.projects_filter_button}>custom solution</a>        
+            </div >
+            <div className={styles.projects_content_descib}>
+                    <div className={styles.projects_content_paragraph}>Blue is perfect. It’s the most popular and the most relaxing colour. Blue is also the base of the color spectrum. No blue — no color: all other colors may be 
+                    created only by mixing blue, red and green. Web is pure blue: from the first site by CERN till now. Why not to create another beatiful site?
+                    </div>
+                    {/* <div className={styles.projects_content_paragraph}> No blue — no color: all other colors may be created only by mixing blue, red and green. Web is pure blue: from the
+                    first site by CERN till now. Why not to create another beatiful site?
+                    </div> */}
+            </div>
+            <div className={styles.projects_button_wrapper}>
+              <a className={styles.projects_button}>
+                Learn More
+              </a>
+            </div>
+            </div>
+            </div>  
+      </section>
+      
+    )
+  }
+  
+  export default Dashboard
--- src/components/Main/projects/projects.jsx
+++ src/components/Main/projects/projects.jsx
@@ -5,6 +5,8 @@ import ARROW from '../../../img/other/9TR5GzG5c.gif'
 
 import Skeptic from './skepic'
 import Oilfaces from './oilfaces'
+import Dashboard from './dashboard'
+import Skelya from './skelya'
 
 
 const Projects = () => {
@@ -15,6 +17,12 @@ const Projects = () => {
         </div>
         <div className={styles.projects}>
           <Oilfaces/>
+        </div>
+        <div className={styles.projects}>
+          <Dashboard/>
+        </div>
+        <div className={styles.projects}>
+          <Skelya/>
         </div>
       </div>
     )
